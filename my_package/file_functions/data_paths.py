47283e58cef1f34c5c9ff50679ae1bc4bce0c86a
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2023-09-27 08:35:04 +0200

    Python 3.7 is no longer supported

--- my_package/file_functions/data_paths.py
+++ my_package/file_functions/data_paths.py
@@ -1,14 +1,7 @@
 from __future__ import annotations
 import pathlib
 import sys
-from typing import TYPE_CHECKING
-
-
-if TYPE_CHECKING:  # pragma: no cover
-    if sys.version_info >= (3, 8):
-        from typing import Final
-    else:
-        from typing_extensions import Final
+from typing import TYPE_CHECKING, Final
 
 
 __all__ = ["ROOT_DIR", "DATA_DIR"]
